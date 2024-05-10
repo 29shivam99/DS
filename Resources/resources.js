62cf4fc4f3153fc0af38de6cadad29b159194917
29shivam99 <shivam29mishra99@gmail.com>
2024-05-10 18:50:21 +0530

    [resources and VanillJs practice solutions by Jaynil] Added

---
+++ Resources/resources.js
@@ -0,0 +1,17 @@
+// Vanilla JS Practice Problems
+https://github.com/Jaynil1611/Vanilla-JS-Practice
+
+// Dcode Vanilla JS Playlist
+https://www.youtube.com/playlist?list=PLVvjrrRCBy2KvTPJ-HLG4PRqYf-MVJ0h7
+
+// Sadanand's Frontend Challenges
+https://github.com/sadanandpai/frontend-mini-challenges
+
+// Frontend Engineer System Design Playlist
+https://www.youtube.com/watch?v=5vyKhm2NTfw&list=PLI9W87-Dqn7j_x6QtR6sUjycJR7nQLBqT&pp=iAQB
+
+// Chirag Goel Chakde System Design Playlist-
+https://www.youtube.com/watch?v=sV_4pOGosnU&list=PL4CFloQ4GGWICE0Tz6iXKfN3XWkXRlboU&pp=iAQB
+
+// Jaynil (Linkedin - https://www.linkedin.com/in/jaynilgaglani/) Vanilla JS Practice
+https://github.com/Jaynil1611/Vanilla-JS-Practice
